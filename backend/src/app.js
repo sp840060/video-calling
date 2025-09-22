310c54c1cf39d887492525c6eacfe80dae1e41c6
sp840060 <sp840060@gmail.com>
2025-09-22 21:56:50 +0530

    upload commit

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "node:http";
+import mongoose from "mongoose";
+import { connectToSocket } from './controllers/socketManager.js';
+import userRoutes from "./routes/users.routes.js";
+
+
+
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}))
+
+
+app.use("/api/v1/users", userRoutes);
+
+
+// app.get("/home", (req, res) => {
+//   return res.json({ hello: "World" });
+// });
+
+const start = async () => {
+  const connectionDb = await mongoose.connect(process.env.MONGODB_URI);
+  console.log(`MONOGO Connected DB:  ${connectionDb.connection.host}`)
+  server.listen(app.get("port"), () => {
+    console.log("Listing on port 8000");
+  });
+};
+start();
